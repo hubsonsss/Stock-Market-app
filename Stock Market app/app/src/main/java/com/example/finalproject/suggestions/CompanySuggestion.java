86dd358a1acbc8b0e1765b66369dcf1ccab2072c
hubsonsss <sobocinskih@gmail.com>
2025-02-04 17:50:54 +0100

    project

---
+++ Stock Market app/app/src/main/java/com/example/finalproject/suggestions/CompanySuggestion.java
@@ -0,0 +1,28 @@
+/*
+package com.example.finalproject.suggestions;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CompanySuggestion {
+    private String name;
+    private String ticker;
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "pl.pw.edu.mini.zpoif.stockMarket.suggestions.CompanySuggestion{" +
+                "ticker='" + ticker + '\'' +
+                ", name='" + name +
+                '}';
+    }
+}
+*/
---
+++ Stock Market app/app/src/main/java/com/example/finalproject/suggestions/SuggestionAPI.java
@@ -0,0 +1,16 @@
+/*
+package com.example.finalproject.suggestions;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface SuggestionAPI {
+    @GET("utilities/search")
+    Call<List<CompanySuggestion>> getSuggestions(
+            @Query("query") String query,
+            @Query("token") String apiToken);
+}
+*/
